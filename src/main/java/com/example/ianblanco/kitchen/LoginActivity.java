1dec57214123cfd1d7dc24da298c9dcfe6d2d6d3
Kael Moreno <kael.moreno@coreproc.ph>
2016-11-10 14:48:01 +0800

    Add utility for UI and setting of AuthKey from meta-data

--- src/main/java/com/example/ianblanco/kitchen/LoginActivity.java
+++ src/main/java/com/example/ianblanco/kitchen/LoginActivity.java
@@ -10,6 +10,7 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.TextView;
 
 import com.example.ianblanco.kitchen.models.APIError;
 import com.example.ianblanco.kitchen.models.SampleUserCredentials;
@@ -17,9 +18,12 @@ import com.example.ianblanco.kitchen.models.User;
 import com.example.ianblanco.kitchen.utils.ErrorUtil;
 import com.example.ianblanco.kitchen.utils.RestClient;
 import com.example.ianblanco.kitchen.utils.ApiInterface;
+import com.example.ianblanco.kitchen.utils.UiUtil;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonObject;
+
+import org.w3c.dom.Text;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -33,11 +37,14 @@ public abstract class LoginActivity extends AppCompatActivity {
     protected abstract int setLayout();
     protected abstract Context setApplicationContext();
 
-    protected String mBaseUrl;
     public Context mContext;
     private AlertDialog mAlertDialog;
     private ProgressDialog mProgressDialog;
     private boolean mApplicationHasLayout = false;
+
+    // API Values
+    protected String mBaseUrl;
+    protected String mAuthKey;
 
     public interface LoginCallback {
         void onSuccess(User user, JsonObject jsonObject);
@@ -57,6 +64,9 @@ public abstract class LoginActivity extends AppCompatActivity {
         mApplicationHasLayout = setLayout() != 0;
         setApiValues();
 
+        if (!mApplicationHasLayout) {
+
+        }
 //        Toolbar mToolbar = (Toolbar) findViewById(R.id.toolbar);
 //        setSupportActionBar(mToolbar);
 //        // Get the ActionBar here to configure the way it behaves.
@@ -71,7 +81,7 @@ public abstract class LoginActivity extends AppCompatActivity {
 
     private void setApiValues() {
         // Main Application Context
-        // Base URL
+        // Getting Base URL from meta-data
         Context mainApplicationContext = setApplicationContext();
         ApplicationInfo app = null;
         try {
@@ -80,13 +90,25 @@ public abstract class LoginActivity extends AppCompatActivity {
             mBaseUrl = bundle.getString("base-url", "");
 
             if (mBaseUrl.length() == 0) {
-                showAlertDialog("URL not found", "Base URL not found in manifest. Please declare a meta-data value with name \"base-url\".");
+                UiUtil.showAlertDialog(mContext, "URL not found", "Base URL not found in manifest. Please declare a meta-data value with name \"base-url\".");
             }
 
         } catch (PackageManager.NameNotFoundException e) {
             e.printStackTrace();
         }
 
+        try {
+            app = mainApplicationContext.getPackageManager().getApplicationInfo(mainApplicationContext.getPackageName(), PackageManager.GET_META_DATA);
+            Bundle bundle = app.metaData;
+            mAuthKey = bundle.getString("login-auth-key", "");
+
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void initPredefinedFunction() {
 
     }
 
@@ -109,13 +131,12 @@ public abstract class LoginActivity extends AppCompatActivity {
 
     protected void loginFunction(String userName, String password, final LoginCallback callBack) {
 
-        Log.i("tag", userName);
         if (userName.equals("") && password.equals("")) {
             String title = "Error";
             String message = "Please fill required fields";
-            showAlertDialog(title, message, true);
+            UiUtil.showAlertDialog(mContext, title, message, true);
         } else {
-            String auth = "c814048d0ecb678a451f58da18c4897ca8c068b8";
+            String auth = mAuthKey;
             ApiInterface apiInterface = RestClient.getmApiInterface(mBaseUrl);
             SampleUserCredentials userCredentials = new SampleUserCredentials(userName, password);
             Call<JsonObject> call = apiInterface.Login(auth, userCredentials);
@@ -148,45 +169,51 @@ public abstract class LoginActivity extends AppCompatActivity {
         }
     }
 
+    protected void loginFunction(TextView userNameTextView, TextView passwordTextView, final LoginCallback callBack) {
 
-    protected void showAlertDialog(AlertDialog alertDialog) {
-        dismissAlertDialog();
-        mAlertDialog = alertDialog;
-        mAlertDialog.show();
-    }
+        String userName = userNameTextView.getText().toString();
+        String password = passwordTextView.getText().toString();
 
-    protected void showAlertDialog(String title, String message, boolean cancelable) {
-        if (mContext == null) {
-            return;
+        if (userName.equals("") && password.equals("")) {
+            String title = "Error";
+            String message = "Please fill required fields";
+            UiUtil.showAlertDialog(mContext, title, message, true);
+        } else {
+            String auth = "c814048d0ecb678a451f58da18c4897ca8c068b8";
+            ApiInterface apiInterface = RestClient.getmApiInterface(mBaseUrl);
+            SampleUserCredentials userCredentials = new SampleUserCredentials(userName, password);
+            Call<JsonObject> call = apiInterface.Login(auth, userCredentials);
+            call.enqueue(new Callback<JsonObject>() {
+                @Override
+                public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
+                    if (!response.isSuccessful()) {
+                        Log.i("tag", "wrong credentials");
+                        APIError error = ErrorUtil.parsingError(response);
+                        callBack.onError(error.getError());
+                        return;
+                    }
+                    Log.i("tag", "success");
+                    Log.i("json", "response:" + response.body());
+
+                    User user = new Gson().fromJson(response.body().get("data").getAsJsonObject(), User.class);
+
+                    callBack.onSuccess(user, response.body());
+
+                }
+
+                @Override
+                public void onFailure(Call<JsonObject> call, Throwable t) {
+
+                    callBack.onFailed();
+                }
+            });
+
+
         }
-        AlertDialog alertDialog = new AlertDialog.Builder(mContext)
-                .setTitle(title)
-                .setMessage(message)
-                .setCancelable(cancelable)
-                .setPositiveButton("OK",
-                        new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialogInterface, int i) {
-                            }
-                        })
-                .create();
-        showAlertDialog(alertDialog);
     }
 
-    protected void showAlertDialog(String title, String message) {
-        showAlertDialog(title, message, false);
-    }
 
-    protected void showAlertDialog(String message) {
-        showAlertDialog("", message, false);
-    }
 
-    protected void dismissAlertDialog() {
-        if (mAlertDialog != null && mAlertDialog.isShowing()) {
-            mAlertDialog.dismiss();
-        }
-        mAlertDialog = null;
-    }
 
 
 }
---
+++ src/main/java/com/example/ianblanco/kitchen/utils/UiUtil.java
@@ -0,0 +1,52 @@
+package com.example.ianblanco.kitchen.utils;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+
+/**
+ * Created by Kael on 11/10/2016.
+ */
+
+public class UiUtil {
+
+    public static  void showAlertDialog(AlertDialog alertDialog) {
+
+        dismissAlertDialog(alertDialog);
+        alertDialog.show();
+    }
+
+    public static  void showAlertDialog(Context context, String title, String message, boolean cancelable) {
+        if (context == null) {
+            return;
+        }
+        AlertDialog alertDialog = new AlertDialog.Builder(context)
+                .setTitle(title)
+                .setMessage(message)
+                .setCancelable(cancelable)
+                .setPositiveButton("OK",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                            }
+                        })
+                .create();
+        showAlertDialog(alertDialog);
+    }
+
+    public static  void showAlertDialog(Context context, String title, String message) {
+        showAlertDialog(context, title, message, false);
+    }
+
+    public static  void showAlertDialog(Context context, String message) {
+        showAlertDialog(context, "", message, false);
+    }
+
+    public static void dismissAlertDialog(AlertDialog alertDialog) {
+        if (alertDialog != null && alertDialog.isShowing()) {
+            alertDialog.dismiss();
+        }
+        alertDialog = null;
+    }
+
+}
