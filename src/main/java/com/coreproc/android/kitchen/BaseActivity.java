73a82e38a545be78303a8a7afb3ba441c2c23d90
Kael Moreno <kael.moreno@coreproc.ph>
2016-11-11 14:27:39 +0800

    Added more global request calls

---
+++ src/main/java/com/coreproc/android/kitchen/BaseActivity.java
@@ -0,0 +1,19 @@
+package com.coreproc.android.kitchen;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Kael on 11/11/2016.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
--- src/main/java/com/coreproc/android/kitchen/utils/ApiInterface.java
+++ src/main/java/com/coreproc/android/kitchen/utils/ApiInterface.java
@@ -3,10 +3,14 @@ package com.coreproc.android.kitchen.utils;
 import com.coreproc.android.kitchen.models.LoginCredentials;
 import com.google.gson.JsonObject;
 
+import java.util.HashMap;
+
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
 import retrofit2.http.Url;
 
 /**
@@ -23,6 +27,26 @@ public interface ApiInterface {
     @POST()
     Call<JsonObject> SignUp(
             @Url String url, @Header(AUTHORIZATION) String authorization, @Body LoginCredentials userCredentials);
+
+    @POST()
+    Call<JsonObject> PostRequest(
+            @Url String url, @Header(AUTHORIZATION) String authorization, @Body HashMap<String, Object> query);
+
+    @POST()
+    Call<JsonObject> PostRequest(
+            @Url String url, @Header(AUTHORIZATION) String authorization, @Body JsonObject jsonObject);
+
+    @GET()
+    Call<JsonObject> GetRequest(
+            @Url String url, @Header(AUTHORIZATION) String authorization, @Body HashMap<String, Object> query);
+
+    @PUT()
+    Call<JsonObject> PutRequest(
+            @Url String url, @Header(AUTHORIZATION) String authorization, @Body HashMap<String, Object> query);
+
+    @POST()
+    Call<JsonObject> PutRequest(
+            @Url String url, @Header(AUTHORIZATION) String authorization, @Body JsonObject jsonObject);
 
 }
 
